ceb0c6148338c80a60ed94a7df037b016da469b2
nassbuk <anassf700@gmail.com>
2023-01-13 19:20:44 +0000

    p3

---
+++ src/test.ts
@@ -0,0 +1,17 @@
+let a : number;
+let b : boolean;
+let c : string;
+
+type User ={
+    name : string;
+    surname: string;
+    age: number;
+    isAdmin : boolean;
+};
+
+const user : User ={
+    name:'nick',
+    surname:'anass',
+    age : 16,
+    isAdmin:true,
+};
